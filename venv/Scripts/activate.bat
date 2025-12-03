853b0fa53d376411b083e631587758cb033e2cb0
Samuel <samucanik@edu.unifor.br>
2025-12-03 12:00:24 -0300

    vendo erro no back

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\pimba\OneDrive\Área de Trabalho\Market_Albion_Online\venv"
+set "VIRTUAL_ENV=C:\Users\pimba\OneDrive\Área de Trabalho\albion\Market_Albion_Online\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
